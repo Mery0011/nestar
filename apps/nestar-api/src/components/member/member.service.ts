56b8b5d175da95f3a677c730dd45c5d4797f153b
Mery <nasibajahon1995@gmail.com>
2026-08-20 20:38:59 +0900

    fix: creation and verification jwt

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -14,11 +14,10 @@ export class MemberService {
     ) { }
 
     public async signup(input: MemberInput): Promise<Member> {
-        //TODO hash password
         input.memberPassword = await this.authService.hashPassword(input.memberPassword);
         try {
             const result = await this.memberModel.create(input);
-            //TODO: Authentication via TOKEN
+            result.accessToken = await this.authService.createToken(result);
             return result;
         } catch (err) {
             console.log('Error, Service.model:', (err as Error).message);
@@ -37,10 +36,9 @@ export class MemberService {
         } else if (response.memberStatus === MemberStatus.BLOCK) {
             throw new InternalServerErrorException(Message.BLOCKED_USER);
         }
-
         const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword);
-
         if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+        response.accessToken = await this.authService.createToken(response);
         return response;
     }
 
